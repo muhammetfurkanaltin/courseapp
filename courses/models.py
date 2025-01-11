91c3784cbebda2945056b4874b11490c2c2f1958
Muhammet Furkan ALTIN <m.furkangold@gmail.com>
2025-01-11 15:50:41 +0300

    6.1

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,13 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
 class Course(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
@@ -10,6 +17,7 @@ class Course(models.Model):
     date = models.DateField()
     isActive = models.BooleanField()
     slug = models.SlugField(default='',blank=True,editable=False,null=False, unique=True, db_index=True)
+    category = models.ForeignKey(Category,default=1, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -18,10 +26,3 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=40)
-    slug = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
