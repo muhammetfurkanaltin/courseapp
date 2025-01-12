1c99a5c8a9d18e4b205974a1bd1f87dda3d57309
Muhammet Furkan ALTIN <m.furkangold@gmail.com>
2025-01-12 23:36:58 +0300

    7.3

--- courses/admin.py
+++ courses/admin.py
@@ -3,11 +3,11 @@ from .models import Course, Category
 # Register your models here.
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ("title","isActive","slug","category_list",)
+    list_display = ("title","isActive","isHome","slug","category_list",)
     list_display_links = ("title","slug")
     prepopulated_fields = {"slug":("title",)}
-    list_filter = ("isActive",)
-    list_editable = ("isActive",)
+    list_filter = ("isActive","isHome")
+    list_editable = ("isActive","isHome")
     search_fields = ("title","description")
 
     def category_list(self,obj):
--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render , redirect
 from .models import Course , Category
 from django.core.paginator import Paginator
 
 def index(request):
     #list comprehension
-    kurslar = Course.objects.filter(isActive=1)
+    kurslar = Course.objects.filter(isActive=1,isHome=1)
     
     kategoriler = Category.objects.all()
 
@@ -22,13 +22,25 @@ def details(request,slug):
     }
     return render(request, 'courses/details.html',context)
 
+def search(request):
+    if "q" in request.GET and request.GET["q"] != "":
+        q = request.GET["q"]
+        kurslar = Course.objects.filter(isActive=True,title__contains=q).order_by('-id')
+        kategoriler = Category.objects.all()
+    else:
+        return redirect('/kurslar')
+    return render(request, 'courses/search.html',{
+        'categories':kategoriler,
+        'courses': kurslar,
+        
+    })
 def getCoursesByCategory(request, slug):
     kurslar = Course.objects.filter(categories__slug=slug, isActive=True).order_by('-id')
     kategoriler = Category.objects.all()
     paginator = Paginator(kurslar, 2)
     page = request.GET.get('page',1)
     page_obj = paginator.page(page)
-    return render(request, 'courses/index.html',{
+    return render(request, 'courses/list.html',{
         'categories':kategoriler,
         'page_obj': page_obj,
         'seciliKategori':slug
